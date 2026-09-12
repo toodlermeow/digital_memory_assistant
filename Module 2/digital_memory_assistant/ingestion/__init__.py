a8f70efdbc3193c6efa387f63c2c823538732a0e
toodler <toodlermeow@gmail.com>
2026-09-12 23:23:34 +0530

    Complete Module 2 ingestion package init

--- Module 2/digital_memory_assistant/ingestion/__init__.py
+++ Module 2/digital_memory_assistant/ingestion/__init__.py
@@ -1,6 +1,1 @@
-"""
-The `ingestion` package: file ingestion and validation for the
-Digital Memory Assistant project.
-
-Public entry point: FileHandler.ingest_file()
-"""
+"""File ingestion and validation package."""
